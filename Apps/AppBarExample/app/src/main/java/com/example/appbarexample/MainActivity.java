fd0542f9bfbd7b1caefc1e2f8ab408c313e27485
unknown <kw577@wp.pl>
2019-03-19 22:21:42 +0100

    Menu bar - korzystanie z android icons (dodac do folderu app/res/mipmap), rozwijana lista menu (app:showAsAction=never), click events

--- Apps/AppBarExample/app/src/main/java/com/example/appbarexample/MainActivity.java
+++ Apps/AppBarExample/app/src/main/java/com/example/appbarexample/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.appbarexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +23,41 @@ public class MainActivity extends AppCompatActivity {
 
         //ustawienie zaprojektowanego paska narzedzi jako glownego paska narzedzi aplikacji
         setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.app_bar_menu, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //handling click events
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.action_share:
+                Toast.makeText(this, "Share option selected...", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_settings:
+                Toast.makeText(this, "Settings option selected...", Toast.LENGTH_SHORT).show();
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+
+
     }
 }
