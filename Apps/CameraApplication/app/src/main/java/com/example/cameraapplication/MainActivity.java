d584fe11a54e3a5be8190c6b82e74f745194ce7b
unknown <kw577@wp.pl>
2019-04-02 22:17:39 +0200

    CameraApplication - wykonananie i pobranie zdjecia wbudowanym aparatem

--- Apps/CameraApplication/app/src/main/java/com/example/cameraapplication/MainActivity.java
+++ Apps/CameraApplication/app/src/main/java/com/example/cameraapplication/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.cameraapplication;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView =findViewById(R.id.imageView);
+    }
+
+    public void takePicture(View view) {
+
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(imageTakeIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+    //pobranie i ustawienie obrazka w widoku aplikacji
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+
+
     }
 }
