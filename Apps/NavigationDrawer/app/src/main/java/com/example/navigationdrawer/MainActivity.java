b53a3f48337a325b8b7139fd94268f0a4c06f6fb
unknown <kw577@wp.pl>
2019-04-02 21:26:29 +0200

    NawigationDrawer - dodanie dodatkowego przyciku w aplikacji ktory powoduje rozwiniecie paska narzedzi

--- Apps/NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ Apps/NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.navigationdrawer;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -23,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+
+        //dodanie przycisku do paska aplikacji
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
 
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -74,4 +81,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return  true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
